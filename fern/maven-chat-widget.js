f75a469d17412d8bc3c3631c6b11b2d49768b78c
Amol Bhave <amol@amolbhave.com>
2024-06-06 08:29:41 -0500

    initial commit

---
+++ fern/maven-chat-widget.js
@@ -0,0 +1,9 @@
+var script = document.createElement('script')
+script.src = 'https://www.mavenwidgets.net/widget.js'
+script.async = true
+script.onload = function () {
+  Maven.ChatWidget.load({
+    apiKey: 'aak_9Bh6ouMdVFvAOVHJE3ZQOQThbtqEsGjpp4B1ZtYLyPwvL6AraHCYu0pf1Tz9eJ3nPaS',
+  })
+}
+document.head.appendChild(script)
